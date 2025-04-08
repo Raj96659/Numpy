08c9e8aeea4574d960152f2332e005d0551f8c13
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-04-08 13:06:18 +0530

    Create C_Numpy_Properties.py

---
+++ C_Numpy_Properties.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+arr = np.array([[1,2,3],[4,5,6]])
+
+print("Shape of Numpy array : ")
+print(arr.shape)
+
+print("\n")
+
+print("Size of Numpy Array : ")
+print(arr.size)
+
+print("\n")
+
+print("Dimension of Numpy Array : ")
+print(arr.ndim)
+
+print("\n")
+
+print("Datatype of Numpy Array : ")
+print(arr.dtype)
+
+print("\n")
+
+print("If you want to change the datatype : ")
+# new_arr = arr.astype(float)
+# print(new_arr)
