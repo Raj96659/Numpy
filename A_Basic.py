f7e0826191de67c0e32b39919064bef3b550c00f
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-04-08 13:03:51 +0530

    Create A_Basic.py

---
+++ A_Basic.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr = np.array([1,2,3,4])
+print(arr)
+
+print("\n")
+
+print("One dimensional array : ")
+arr1D = np.array([1,2,3,4,5])
+print(arr1D)
+
+print("\n")
+
+arr2D = np.array([[1,2,3],[4,5,6]])
+print("Two dimensional array : ")
+print(arr2D)
